f36304c367cea8c666f3dd6efc3a250ef163497b
Pablo <pablo.munoz.sommaruga@gmail.com>
2023-02-02 23:34:11 -0300

    New article changes

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -12,8 +12,37 @@ class ArticlesController < ApplicationController
   end
 
   def create
+    @article = Article.new(article_params)
+
+    if @article.save
+      redirect_to @article
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
 
   def edit
+    @article = Article.find(params[:id])
+  end
+
+  def update
+    @article = Article.find(params[:id])
+
+    if @article.update(article_params)
+      redirect_to @article
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+    redirect_to articles_path, status: :see_other
+  end
+
+  private
+  def article_params
+    params.require(:article).permit(:title, :content)
   end
 end
